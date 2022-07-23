72058c6cccfa95c60402914bbc2add66fc669569
Patrik <57401452+Patrik157@users.noreply.github.com>
2022-07-23 17:44:19 +0200

    Revert "Frontend dev"

--- frontend/hackathon/src/app/user.service.ts
+++ frontend/hackathon/src/app/user.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@angular/core';
 import { User } from './user';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-import { catchError, map, tap} from 'rxjs/operators';
-import { User } from './user';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -20,6 +16,6 @@ export class UserService {
     const url = this.userUrl + "/Register";
     return this.http.post<User>(url, user, this.httpOptions);
   }
-  constructor(private http: HttpClient) {}
 
+  constructor(private http: HttpClient) { }
 }
