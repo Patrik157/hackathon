92ac85f83b48fedbfa61f5c446a93ae0c1881e81
Patrik <thedeadlycookie157@gmail.com>
2022-07-23 12:42:17 +0200

    Build

--- backend/CleanStreetsApi/Models/CleanStreetsDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace CleanStreetsApi.Models;
-
-public class CleanStreetsDatabaseSettings{
-    public string ConnectionString { get; set; } = null!;
-
-    public string DatabaseName { get; set; } = null!;
-
-    public string MapsCollectionName { get; set; } = null!;
-}
