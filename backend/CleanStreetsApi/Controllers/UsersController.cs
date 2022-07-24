cb2868ad993e7edf957d79d80cb0b7f0c7169bb5
Patrik <thedeadlycookie157@gmail.com>
2022-07-24 08:02:06 +0200

    auth removed

--- backend/CleanStreetsApi/Controllers/UsersController.cs
+++ backend/CleanStreetsApi/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CleanStreetsApi.Controllers;
 
-[Authorize]
+//[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
