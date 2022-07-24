ce072dbdc8657604163d9eaf9eae1dd43e3b5677
TihomirPop <tihomirpopovic42@gmail.com>
2022-07-24 05:41:35 +0200

    commit

--- frontend/hackathon/src/app/user-search/user-search.component.ts
+++ frontend/hackathon/src/app/user-search/user-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { User } from '../user';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-search',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-search.component.css']
 })
 export class UserSearchComponent implements OnInit {
+  users$!: Observable<User[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.users$ = this.searchTerms.pipe(
+      debounceTime(300),        //ceka 300ms prije novog prosljedivanja
+      distinctUntilChanged(),   //prosljeduje samo ako je drugacije nego zadnji put
+      switchMap((term: string) => this.userService.searchHeroes(term))  //promjeni prikazane rezultate
+    );
   }
 
 }
